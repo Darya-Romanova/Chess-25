9118f42203c89c42f2ff2cc1459c914457e1e0ef
Darya Romanova <radio_android@mail.ru>
2023-06-04 15:16:24 +0300

    Ex.3 - class Pawn

---
+++ src/Pawn.java
@@ -0,0 +1,39 @@
+public class Pawn extends ChessPiece{
+    public Pawn(String color) {
+        super(color);
+    }
+
+    @Override
+    public String getColor() {
+        return this.color;
+    }
+
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (column == toColumn) {
+            int dir;
+            int start;
+
+            if (color.equals("White")) {
+                    dir = 1;
+                    start = 1;
+            } else {
+                dir = -1;
+                start = 6;
+            }
+
+            if (line + dir == toLine) {
+                return chessBoard.board[toLine][toColumn] == null;
+            }
+
+            if (line == start && line + 2 * dir == toLine) {
+                return chessBoard.board[toLine][toColumn] == null && chessBoard.board[line + dir][column] == null;
+            }
+        } return false;
+    }
+
+    @Override
+    public String getSymbol() {
+        return "P";
+    }
+}
